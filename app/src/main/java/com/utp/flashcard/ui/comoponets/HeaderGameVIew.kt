19168405ccbe16d4d23dad9e51154d269acb2cd6
Wigudunibaler <wigudunibalera@gmail.com>
2024-06-24 18:05:11 -0500

    Create Header for CardView

---
+++ app/src/main/java/com/utp/flashcard/ui/comoponets/HeaderGameVIew.kt
@@ -0,0 +1,69 @@
+package com.utp.flashcard.ui.comoponets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.utp.flashcard.R
+
+@Composable
+fun HeaderGameView() {
+    Row (
+        modifier =  Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        ArrowBack()
+        TitleCardGame(title = "Verbos" )
+        Spacer(Modifier.size(10.dp))
+    }
+}
+
+@Composable
+fun TitleCardGame(title :String ) {
+    Text(
+        text = title,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        color = colorResource(id = R.color.white)
+    )
+}
+
+@Composable
+fun ArrowBack() {
+    Box (
+        modifier = Modifier.padding(start = 30.dp)
+    ){
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                tint = colorResource(id = R.color.white),
+                painter = painterResource(id = R.drawable.icon_arrow_back),
+                contentDescription = "Icon arrow back",
+
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+
+fun PreviewHeaderGameView() {
+    HeaderGameView()
+}
